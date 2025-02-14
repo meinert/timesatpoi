235250366cf6925bfbd88a5558a0c73396fca2ec
meinert <ppoulsen@gmail.com>
2025-02-14 12:03:35 +0100

    Update runme.py

--- runme.py
+++ runme.py
@@ -14,18 +14,21 @@ locale.setlocale(locale.LC_ALL, '')  # Use '' for auto, or force e.g. to 'en_US.
 # USER INPUT
 #############
 
-json_file = "Records.json"
+json_file = "Records2024.json"
 
 # Get first and last timestamps of interest
-begin_ts = helpers.date_ymd_to_timestamp_ms(2021, 1, 1)
-end_ts = helpers.date_ymd_to_timestamp_ms(2021, 12, 31)
+begin_ts = helpers.date_ymd_to_timestamp_ms(2022, 1, 1)
+end_ts = helpers.date_ymd_to_timestamp_ms(2022, 12, 31)
 
-begin_date = datetime.date(year=2021, month=1, day=1)
-end_date = datetime.date(year=2021, month=12, day=31)
+begin_date = datetime.date(year=2023, month=1, day=1)
+end_date = datetime.date(year=2023, month=12, day=31)
 
 # Point of interest
-poi = np.array([56.71370324568134,10.113683819381745])    # in degrees
-radius_max = 500                         # in meters
+    poi = np.array([56.71370324568134, 10.113683819381745])    # in degrees Hadsund
+# poi = np.array([57.01252757476267, 9.987804015778735])    # in degrees Aalborg
+
+
+radius_max = 1000                         # in meters
 
 # Define the interval of time below which timestamps should be grouped together
 group_size = datetime.timedelta(weeks=0, days=1, hours=0, minutes=0, seconds=0, milliseconds=0)
